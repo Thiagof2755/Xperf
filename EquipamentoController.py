2bd9b538da1b9cc9eb5dbb560d3e373ce01e4479
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2023-08-11 16:33:36 -0300

    db modify

--- EquipamentoController.py
+++
@@ -1,9 +0,0 @@
-from View import View
-
-
-class EquipamentoController:
-    def __init__(self):
-        self.view = View()
-
-    def run(self):
-        return self.view.input_equipment_info()
--- database_functions.py
+++ database_functions.py
@@ -1,56 +1,53 @@
 import mysql.connector
 from datetime import datetime
+import PySimpleGUI as sg
 
-def input_equipment_info():
-    print("\nINFORMAÇÕES DO EQUIPAMENTO:")
-    modelo = input("Digite o MODELO: ")
-    marca = input("Digite a MARCA: ")
-    mac = input("Digite o MAC: ")
-    return modelo, marca, mac
-def insert_data_to_db(host, user, password, database, equip_info, ping_results, speed_results):
-    modelo, marca, mac = equip_info
+class EquipmentInfo:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.window = None
 
-    try:
-        # Estabelecer conexão com o banco de dados
-        db_connection = mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database
-        )
+    def insert_data_to_db(self, equip_info, ping_results, speed_results):
+        modelo, marca, mac = equip_info
 
-        # Criar o cursor para executar as operações SQL
-        cursor = db_connection.cursor()
+        try:
+            db_connection = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
 
-        # Incluir os dados da tabela Equipamento
-        insert_equipment_query = "INSERT INTO equipamento (marca, modelo, mac) VALUES (%s, %s, %s)"
-        equipment_values = (marca, modelo, mac)
-        cursor.execute(insert_equipment_query, equipment_values)
+            cursor = db_connection.cursor()
 
-        # Obter o ID do equipamento inserido
-        equip_id = cursor.lastrowid
+            insert_equipment_query = "INSERT INTO equipamento (marca, modelo, mac) VALUES (%s, %s, %s)"
+            equipment_values = (marca, modelo, mac)
+            cursor.execute(insert_equipment_query, equipment_values)
 
-        # Incluir os dados da tabela Ping para ms1, ms2 e ms3
-        insert_ping_query = "INSERT INTO ping (equipamento, ms1, ms2, ms3, data_hora_ping) VALUES (%s, %s, %s, %s, %s)"
-        ping_values = [(equip_id, ms1, ms2, ms3, data_hora_ping) for ms1, ms2, ms3, data_hora_ping in ping_results]
-        cursor.executemany(insert_ping_query, ping_values)
+            equip_id = cursor.lastrowid
 
-        # Incluir os dados da tabela SpeedTest para download, upload e data_hora_speedtest
-        insert_speed_query = "INSERT INTO speedtest (equipamento, download_speed, upload_speed, data_hora_speedtest) VALUES (%s, %s, %s, %s)"
-        speed_values = [(equip_id, download_speed, upload_speed, data_hora_speedtest) for download_speed, upload_speed, data_hora_speedtest in speed_results]
-        cursor.executemany(insert_speed_query, speed_values)
+            insert_ping_query = "INSERT INTO ping (equipamento, ms1, ms2, ms3, data_hora_ping) VALUES (%s, %s, %s, %s, %s)"
+            ping_values = [(equip_id, ms1, ms2, ms3, data_hora_ping) for ms1, ms2, ms3, data_hora_ping in ping_results]
+            cursor.executemany(insert_ping_query, ping_values)
 
-        # Efetivar as alterações no banco de dados
-        db_connection.commit()
+            insert_speed_query = "INSERT INTO speedtest (equipamento, download_speed, upload_speed, data_hora_speedtest) VALUES (%s, %s, %s, %s)"
+            speed_values = [(equip_id, download_speed, upload_speed, data_hora_speedtest) for download_speed, upload_speed, data_hora_speedtest in speed_results]
+            cursor.executemany(insert_speed_query, speed_values)
 
-        print("\nDados inseridos no banco de dados com sucesso!")
+            db_connection.commit()
 
-    except mysql.connector.Error as error:
-        print(f"Erro ao conectar ao banco de dados: {error}")
+            print("\nDados inseridos no banco de dados com sucesso!")
 
-    finally:
-        # Fechar cursor e conexão
-        if cursor:
-            cursor.close()
-        if db_connection:
-            db_connection.close()
+        except mysql.connector.Error as error:
+            print(f"Erro ao conectar ao banco de dados: {error}")
+
+        finally:
+            if cursor:
+                cursor.close()
+            if db_connection:
+                db_connection.close()
+
+
