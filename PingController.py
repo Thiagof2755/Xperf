e7d377d1c9703a2989ebe5b0e45c01b1e0fda573
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2023-08-14 16:05:47 -0300

    prog bar create

--- PingController.py
+++ PingController.py
@@ -1,6 +1,6 @@
-from PingModel import PingModel
 import threading
-
+import PySimpleGUI as sg
+from PingModel import PingModel
 
 class PingController:
     def __init__(self, destino1, destino2, destino3, num_pings, atraso):
@@ -11,21 +11,59 @@ class PingController:
         self.num_pings = num_pings
         self.atraso = atraso
 
-    def run_ping(self):
-        # Criar as threads
-        thread1 = threading.Thread(target=self.ping.ping_test1, args=(self.destino1, self.num_pings, self.atraso))
-        thread2 = threading.Thread(target=self.ping.ping_test2, args=(self.destino2, self.num_pings, self.atraso))
-        thread3 = threading.Thread(target=self.ping.ping_test3, args=(self.destino3, self.num_pings, self.atraso))
+    def update_progress(self, thread_number, count):
+        if thread_number == 1:
+            self.ping.ping_count1 = count
+        elif thread_number == 2:
+            self.ping.ping_count2 = count
+        elif thread_number == 3:
+            self.ping.ping_count3 = count
 
-        # Iniciar as threads
+    def run_ping(self):
+        # Iniciar as threads de ping
+        thread1 = threading.Thread(target=self.ping.ping_test1, args=(self.destino1, self.num_pings, self.atraso, self.update_progress))
+        thread2 = threading.Thread(target=self.ping.ping_test2, args=(self.destino2, self.num_pings, self.atraso, self.update_progress))
+        thread3 = threading.Thread(target=self.ping.ping_test3, args=(self.destino3, self.num_pings, self.atraso, self.update_progress))
+
+        # Criar a janela de visualização
+        layout = [
+            [sg.Text("Andamento das Threads")],
+            [sg.Text("Thread 1:"), sg.ProgressBar(self.num_pings, orientation='h', size=(50, 20), key='progress1')],
+            [sg.Text("Thread 2:"), sg.ProgressBar(self.num_pings, orientation='h', size=(50, 20), key='progress2')],
+            [sg.Text("Thread 3:"), sg.ProgressBar(self.num_pings, orientation='h', size=(50, 20), key='progress3')],
+            [sg.Button("Fechar")]
+        ]
+        window = sg.Window("Visualização de Threads", layout)
+
+        # Iniciar as threads de ping
         thread1.start()
         thread2.start()
         thread3.start()
+
+        # Loop de atualização da janela de visualização
+        while True:
+            event, values = window.read(timeout=100)  # Atualiza a cada 100ms
+            if event == sg.WIN_CLOSED or event == "Fechar":
+                break
+            # Atualizar as barras de progresso com o progresso de cada thread
+            window['progress1'].update(self.ping.ping_count1)
+            window['progress2'].update(self.ping.ping_count2)
+            window['progress3'].update(self.ping.ping_count3)
 
         # Aguardar até que todas as threads terminem
         thread1.join()
         thread2.join()
         thread3.join()
+        window.close()
+
+        # Criar uma nova janela indicando que o teste foi finalizado
+        layout_final = [
+            [sg.Text("Teste Finalizado!")],
+            [sg.Button("OK")]
+        ]
+        window_final = sg.Window("Teste Finalizado", layout_final)
+        event, values = window_final.read()
+        window_final.close()
 
         return (
             self.ping.resultados_tempo1,
@@ -33,3 +71,4 @@ class PingController:
             self.ping.resultados2,
             self.ping.resultados3
         )
+
--- PingModel.py
+++ PingModel.py
@@ -8,8 +8,11 @@ class PingModel:
         self.resultados1 = []
         self.resultados2 = []
         self.resultados3 = []
+        self.ping_count1 = 0  # Progresso da Thread 1
+        self.ping_count2 = 0  # Progresso da Thread 2
+        self.ping_count3 = 0  # Progresso da Thread 3
 
-    def ping_test1(self, destino1, num_pings, atraso):
+    def ping_test1(self, destino1, num_pings, atraso, progress_callback):
         for _ in range(num_pings):
             tempo_resposta = ping3.ping(destino1)
             tempo_atual = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
@@ -17,20 +20,26 @@ class PingModel:
                 tempo_resposta = -0.001
             self.resultados_tempo1.append(tempo_atual)
             self.resultados1.append(int(tempo_resposta * 1000))
+            self.ping_count1 += 1  # Atualizar o progresso da Thread 1
+            progress_callback(1, self.ping_count1)  # Atualizar progresso na GUI
             time.sleep(atraso)
 
-    def ping_test2(self, destino2, num_pings, atraso):
+    def ping_test2(self, destino2, num_pings, atraso, progress_callback):
         for _ in range(num_pings):
             tempo_resposta = ping3.ping(destino2)
             if tempo_resposta is False or tempo_resposta >= 500:
                 tempo_resposta = -0.001
             self.resultados2.append(int(tempo_resposta * 1000))
+            self.ping_count2 += 1  # Atualizar o progresso da Thread 2
+            progress_callback(2, self.ping_count2)  # Atualizar progresso na GUI
             time.sleep(atraso)
 
-    def ping_test3(self, destino3, num_pings, atraso):
+    def ping_test3(self, destino3, num_pings, atraso, progress_callback):
         for _ in range(num_pings):
             tempo_resposta = ping3.ping(destino3)
             if tempo_resposta is False or tempo_resposta >= 500:
                 tempo_resposta = -0.001
             self.resultados3.append(int(tempo_resposta * 1000))
+            self.ping_count3 += 1  # Atualizar o progresso da Thread 3
+            progress_callback(3, self.ping_count3)  # Atualizar progresso na GUI
             time.sleep(atraso)
